5d2ce029d9a8de1dadd50cc27094d19bdb5acf50
Kwangil Choi <gadje@naver.com>
2024-08-01 17:49:54 +0900

    first commit

---
+++ express.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/user/:id', function (req, res) {
+  const q = req.query
+  console.log(q);
+})
+
+app.listen(3000)
+
+// http://localhost:3000/user/school?group=korea&key[name]=choi&key[age]=20
